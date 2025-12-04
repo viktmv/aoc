507aa6b3bb49a2ac96a94c2ef67be2630b84dcf1
Viktor T. <vtymofeev@wavelo.com>
2025-12-04 10:17:38 -0500

    Update day3

---
+++ 2025/day3/main.go
@@ -0,0 +1,62 @@
+package main
+
+import (
+	"bufio"
+	"log"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	f, err := os.Open("input_test")
+
+	if err != nil {
+		log.Fatalf("could not open file %e", err)
+	}
+
+	defer f.Close()
+
+	scanner := bufio.NewScanner(f)
+	banks := [][]string{}
+	for scanner.Scan() {
+		line := scanner.Text()
+		bank := strings.Split(line, "")
+		banks = append(banks, bank)
+	}
+
+	log.Printf("banks %v\n", banks)
+
+    sum := 0
+	// day1
+	// for i := range(banks) {
+	// 	bank := banks[i]
+	// 	maxPair := 0
+	// 	for j := range(bank) {
+	// 		for k := j+1; k < len(bank); k++ {
+	// 			pair := mustAtoI(bank[j] + bank[k])
+	// 			if pair > maxPair {
+	// 				maxPair = pair
+	// 			}
+	// 		}
+	// 	}
+	// 	sum += maxPair
+	// 	log.Printf("maxPair: %d", maxPair)
+	// }
+
+	// day 2
+
+	log.Printf("sum: %d", sum)
+}
+
+func mustAtoI(s string) int {
+	r, err := strconv.Atoi(s)
+	if err != nil {
+		log.Fatalf("could not atoi with %s", err)
+	}
+	return r
+}
+
+func toIntPair(s1, s2 int) int {
+	return mustAtoI(strconv.Itoa(s1) + strconv.Itoa(s2))
+}
